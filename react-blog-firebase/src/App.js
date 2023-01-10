e913dbe949620ebf109b0c75f037f33fd78637e0
KaliGurl <kaligurl2008@gmail.com>
2023-01-10 16:53:45 -0500

    installed react-router-dom, react-icons, and firebase, fleshed out the header

--- react-blog-firebase/src/App.js
+++ react-blog-firebase/src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Header from './Components/Header/Header';
+import Homepage from './Pages/Homepage/Homepage';
+
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
